1622f173438ac6c752baea9b103d887f1a63bcbc
sakaci <furkan.sakaci@gohm.com.tr>
2020-10-17 18:28:56 +0300

    Limit button has been added

--- control_panel/control_panel.py
+++ control_panel/control_panel.py
@@ -21,7 +21,7 @@ class control_panel():
         
 
     def trainingPanel(self):
-        training_frame=self.widgets.createFrame(self.panel_frame,x=237,y=140,width=400,height=220,anchor="center")
+        training_frame=self.widgets.createFrame(self.panel_frame,x=230,y=120,width=400,height=200,anchor="center")
         training_frame.config(bg="gray70")
 
         title1_label=self.widgets.createLabel(training_frame,"Yeni Eğitim Modeli Üret",200,20,font=("Times New Roman", 16),anchor="center")
@@ -30,40 +30,42 @@ class control_panel():
         variable = tk.StringVar(training_frame)
         variable.set(OptionList[0])
 
-        test_type=self.widgets.createDropdown(training_frame,variable,OptionList,200,80,width=18,height=1)
+        test_type=self.widgets.createDropdown(training_frame,variable,OptionList,100,50,width=18,height=1)
         test_type.config(bg="gray60") 
 
-        file_button=self.widgets.createButton(training_frame,"Dosya Seç",200,130,width=20,height=1,command=self.browseFiles)
+        file_button=self.widgets.createButton(training_frame,"Dosya Seç",100,100,width=20,height=1,command=self.browseFiles)
         file_button.config(bg="gray60")
 
 
-        draw_button=self.widgets.createButton(training_frame,"Model Oluştur",200,180,width=20,height=1,command=self.train)
+        draw_button=self.widgets.createButton(training_frame,"Model Oluştur",100,150,width=20,height=1,command=self.train)
         draw_button.config(bg="gray60") 
 
     def testPanel(self):
-        test_frame=self.widgets.createFrame(self.panel_frame,x=237,y=375,width=400,height=190,anchor="center")
+        test_frame=self.widgets.createFrame(self.panel_frame,x=230,y=350,width=400,height=250,anchor="center")
         test_frame.config(bg="gray70")
 
-        title2_label=self.widgets.createLabel(test_frame,"Emisyon Tahmini Yap",200,20,font=("Times New Roman", 16),anchor="center")
+        title2_label=self.widgets.createLabel(test_frame,"Emisyon Tahmini Yap",190,20,font=("Times New Roman", 16),anchor="center")
         title2_label.config(bg="gray70")
 
         OptionList = ["RE102","CE102"] 
         variable = tk.StringVar(test_frame)
         variable.set(OptionList[0])
 
-        parameter_button=self.widgets.createButton(test_frame,"Parametreleri Gir",200,100,width=20,height=1,command=self.exit)
+        parameter_button=self.widgets.createButton(test_frame,"Parametreleri Gir",100,100,width=20,height=1,command=self.exit)
         parameter_button.config(bg="gray60")
 
         
-        test_type=self.widgets.createDropdown(test_frame,variable,OptionList,200,50,width=18,height=1)
+        test_type=self.widgets.createDropdown(test_frame,variable,OptionList,100,50,width=18,height=1)
         test_type.config(bg="gray60") 
 
         
 
-        predict_button=self.widgets.createButton(test_frame,"Ölçüm Yap",200,150,width=20,height=1,command=self.exit)
+        limit_button=self.widgets.createButton(test_frame,"Limit Seç",100,150,width=20,height=1,command=self.exit)
+        limit_button.config(bg="gray60")
+
+        predict_button=self.widgets.createButton(test_frame,"Ölçüm Yap",100,200,width=20,height=1,command=self.exit)
         predict_button.config(bg="gray60")
 
-        
 
     def train(self):
         self.machine_learning.train(self.filename)
--- widgets/widgets.py
+++ widgets/widgets.py
@@ -11,7 +11,7 @@ class widgets():
         self.tk.config(bg="light slate gray")
         self.logger = utils.get_logger()
 
-    def createButton(self,tk,text,x,y,command=None,width=None,height=None):
+    def createButton(self,tk,text,x,y,command=None,width=None,height=None,anchor=None):
         button = tkinter.Button(tk,text=text,command=command,width=width,height=height)
         button.place(x=x,y=y)
         return button
@@ -26,11 +26,11 @@ class widgets():
         frame.place(x=x,y=y,anchor=anchor)
         return frame
 
-    def createDropdown(self,tk,variable,OptionList,x,y,width=None,height=None):
+    def createDropdown(self,tk,variable,OptionList,x,y,width=None,height=None,anchor=None):
         dropdown=tkinter.OptionMenu(tk,variable,*OptionList)
         dropdown.config(width=width,height=height)
         dropdown["highlightthickness"]=0
-        dropdown.place(x=x,y=y)
+        dropdown.place(x=x,y=y,anchor=anchor)
         return dropdown
 
      
